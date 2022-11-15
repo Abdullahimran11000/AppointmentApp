4b8b66ed7bc50d5fd4fa25df2efcc580b3b4f38a
Abdullah Imran <85618208+Abdullahimran010@users.noreply.github.com>
2022-11-15 17:11:35 +0500

    Update 15-nov-2022
    Splash screen created and added in to app js.

---
+++ src/Screens/Splash.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {SafeAreaView,View,Text} from "react-native";
+
+const Splash = ()=>{
+    return( 
+        <SafeAreaView>
+              <Text>Hello Splash</Text>  
+        </SafeAreaView>
+    )
+};
+
+export default Splash;
