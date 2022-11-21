698c75acf1444c6871e7ce4a5f7c5245edfef531
Hamad Najaf <85858039+hamad-najaf@users.noreply.github.com>
2022-11-21 17:03:36 +0500

    Update App.js

--- App.js
+++ App.js
@@ -1,15 +1,9 @@
 import React from 'react';
 import {SafeAreaView} from 'react-native';
-import Socket from './src/Screens/Socket';
-import Splash from './src/Screens/Splash';
-import LogIn from './src/Screens/LogIn';
 
 const App = () => {
   return (
     <SafeAreaView>
-      {/* <Splash />
-        <Socket /> */}
-        <LogIn></LogIn>
     </SafeAreaView>
   );
 };
