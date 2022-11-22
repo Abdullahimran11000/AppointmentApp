8c5afc6fe32a907e0fa4f9f9caae4eb266d7729d
Abdullah Imran <85618208+Abdullahimran010@users.noreply.github.com>
2022-11-22 15:12:05 +0500

    Updated 22-NOV-2022
    Sign Up screen added.

--- App.js
+++ App.js
@@ -2,23 +2,15 @@
 import React from 'react';
 import {
   SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  Button,
-  useColorScheme,
-  View,
 } from 'react-native';
-import Socket from './src/Screens/Socket';
-import Splash from './src/Screens/Splash';
+import SignUp from './src/Screens/SignUp';
+
 
 const App = () => {
   
 return (
-    <SafeAreaView>
-        <Splash />  
-        <Socket />
+    <SafeAreaView> 
+      <SignUp/>
     </SafeAreaView>
 )}
 
---
+++ src/Screens/SignUp.js
@@ -0,0 +1,147 @@
+import React from 'react';
+import Icon from 'react-native-vector-icons/FontAwesome5';
+import {Checkbox} from 'react-native-paper';
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from 'react-native-responsive-screen';
+import {ScrollView, TouchableOpacity} from 'react-native';
+import {SafeAreaView, View, Text, TextInput, Image} from 'react-native';
+
+const SignUp = () => {
+  return (
+    <ScrollView>
+      <SafeAreaView>
+        <View>
+          <View style={{marginLeft: 150, marginTop: 20}}>
+            <Text style={{fontSize: 20, fontWeight: 'bold', color: 'black'}}>
+              SignUp
+            </Text>
+          </View>
+          <View style={{marginLeft: 30, marginTop: 30}}>
+            <Text style={{fontSize: 15, fontWeight: 'bold', color: 'black'}}>
+              Fullname
+            </Text>
+            <TextInput
+              style={{width: 300, marginTop: 10, borderRadius: 5}}
+              keyboardType="default"
+              placeholder="Enter Your Name"
+              backgroundColor="white"></TextInput>
+          </View>
+          <View style={{marginTop: 20, marginLeft: 30}}>
+            <Text style={{fontSize: 15, fontWeight: 'bold', color: 'black'}}>
+              Email
+            </Text>
+            <TextInput
+              style={{width: 300, marginTop: 10, borderRadius: 5}}
+              keyboardType="email-address"
+              selectionColor={'#c28cde'}
+              placeholder="Enter your Email"
+              backgroundColor="white"></TextInput>
+          </View>
+          <View style={{marginTop: 20, marginLeft: 30}}>
+            <Text style={{fontWeight: 'bold', color: 'black'}}>Password</Text>
+            <TextInput
+              style={{width: 300, marginTop: 10, borderRadius: 5}}
+              keyboardType="default"
+              // maxLength={10}
+              secureTextEntry={true}
+              placeholder="Enter your Password"
+              backgroundColor="white"></TextInput>
+            <Icon
+              style={{
+                position: 'absolute',
+                marginVertical: 46,
+                marginLeft: 270,
+              }}
+              name="eye-slash"
+              size={13}
+              color={'black'}
+            />
+          </View>
+          <View style={{marginLeft: 28, marginTop: 20}}>
+            <Checkbox status="checked" />
+            <Text
+              style={{
+                position: 'absolute',
+                marginHorizontal: 35,
+                marginTop: 8,
+              }}>
+              I agree with
+            </Text>
+            <Text
+              style={{
+                position: 'absolute',
+                marginLeft: 110,
+                marginTop: 8,
+                fontWeight: 'bold',
+                color: 'black',
+              }}>
+              terms
+            </Text>
+            <Text style={{position: 'absolute', marginLeft: 150, marginTop: 8}}>
+              and
+            </Text>
+            <Text
+              style={{
+                position: 'absolute',
+                marginLeft: 177,
+                marginTop: 8,
+                fontWeight: 'bold',
+                color: 'black',
+              }}>
+              privacy
+            </Text>
+          </View>
+          <View
+            style={{
+              width: 230,
+              alignSelf: 'center',
+              marginTop: 30,
+              marginRight: 70,
+            }}>
+            <TouchableOpacity
+              style={{
+                alignItems: 'center',
+                backgroundColor: '#c28cde',
+                width: 300,
+                padding: 10,
+                borderRadius: 20,
+              }}>
+              <Text style={{color: 'white', fontWeight: 'bold'}}>Sign Up</Text>
+            </TouchableOpacity>
+            <Text style={{marginTop: 10, textAlign: 'center', marginLeft: 70}}>
+              Or
+            </Text>
+          </View>
+          <View style={{marginLeft: 30, marginTop: 10}}>
+            <TouchableOpacity
+              style={{
+                backgroundColor: 'white',
+                padding: 10,
+                borderRadius: 20,
+                width: 300,
+              }}>
+              <Image
+                style={{width: 20, height: 20, marginLeft: 77}}
+                source={require('../Assets/images/google.jpg')}
+              />
+              <Text
+                style={{position: 'absolute', marginLeft: 115, marginTop: 10}}>
+                Sign in With this
+              </Text>
+            </TouchableOpacity>
+          </View>
+          <View style={{alignSelf: 'center', marginTop: 10, marginRight: 50}}>
+            <Text>Already have an Account?</Text>
+            <TouchableOpacity style={{position: 'absolute', marginLeft: 165}}>
+              <Text style={{fontWeight: 'bold', color: 'black'}}>Log in</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </SafeAreaView>
+    </ScrollView>
+  );
+};
+
+export default SignUp;
