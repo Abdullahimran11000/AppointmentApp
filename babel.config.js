df48801f18f6b91082e369709ec4c452ca06c3ca
Hamad Najaf <85858039+hamad-najaf@users.noreply.github.com>
2022-11-22 15:29:30 +0500

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,22 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  env: {
+   env: {
     production: {
       plugins: ['react-native-paper/babel'],
     },
   },
+  plugins: [
+    [
+        '@babel/plugin-transform-runtime',
+        {
+            absoluteRuntime: false,
+            corejs: false,
+            helpers: true,
+            regenerator: true,
+            version: '7.0.0-beta.0',
+        },
+    ],
+    'react-native-reanimated/plugin',
+],
 };
+
