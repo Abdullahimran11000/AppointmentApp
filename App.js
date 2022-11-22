12f583f36b3068d576ef8f38fe1bed899781dfc5
Hamad Najaf <85858039+hamad-najaf@users.noreply.github.com>
2022-11-22 15:34:25 +0500

    Update App.js

--- App.js
+++ App.js
@@ -1,17 +1,1 @@
 
-import React from 'react';
-import {
-  SafeAreaView,
-} from 'react-native';
-import SignUp from './src/Screens/SignUp';
-
-
-const App = () => {
-  
-return (
-    <SafeAreaView> 
-      <SignUp/>
-    </SafeAreaView>
-)}
-
-export default App;
