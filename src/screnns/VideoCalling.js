451043d69f20a608b6de9c39f480e9da5c8d7fd0
umarazam7 <181370122@gift.edu.pk>
2022-12-25 14:33:09 +0500

    25 december

--- src/screnns/VideoCalling.js
+++ src/screnns/VideoCalling.js
@@ -8,16 +8,19 @@ import {
 } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import {
-  heightPercentageToDP,
-  widthPercentageToDP,
+  heightPercentageToDP as hp,
+  widthPercentageToDP as wp,
 } from 'react-native-responsive-screen';
 import {VideoCallingStyle} from '../assets/styles/VideoCallingStyle';
 import BackButton from './ScrennHeader/BackButton';
-import MaterialIcon from 'react-native-vector-icons/MaterialIcons'
+import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
+import NeoButton from '../components/NeoMorphButton/NeoButton';
+import {AppColor} from '../assets/colors/AppColor';
 
 const VideoCalling = props => {
   return (
-    <SafeAreaView>
+    <SafeAreaView
+      style={{backgroundColor: AppColor.whiteShade, height: hp('100')}}>
       <BackButton onPress={() => props.navigation.goBack()}></BackButton>
 
       <View style={VideoCallingStyle.nameTimeView}>
@@ -26,7 +29,9 @@ const VideoCalling = props => {
           10:33pm
         </Text>
       </View>
+      
       <View style={VideoCallingStyle.firstCircleView}>
+      
         <View style={VideoCallingStyle.secondCircleView}>
           <View style={VideoCallingStyle.thirdCircleView}>
             <View style={VideoCallingStyle.imageView}>
@@ -37,31 +42,54 @@ const VideoCalling = props => {
             </View>
           </View>
         </View>
-      </View>
+        </View>
+       
 
       <View style={VideoCallingStyle.footerView}>
         <View style={VideoCallingStyle.buttonView}>
-          <TouchableOpacity>
-            <Icon
-              style={{color: 'white'}}
-              name="microphone-slash"
-              size={20}></Icon>
-          </TouchableOpacity>
+          <NeoButton
+            width={wp('17')}
+            height={hp('9')}
+            backgroundColor={AppColor.whiteShade}
+            borderRadius={wp('25')}>
+            <TouchableOpacity>
+              <Icon
+                style={{color: AppColor.black}}
+                name="microphone-slash"
+                size={22}></Icon>
+            </TouchableOpacity>
+          </NeoButton>
         </View>
 
-        <View style={[VideoCallingStyle.buttonView,{backgroundColor:"red"}]}>
-          <TouchableOpacity>
-            <MaterialIcon
-              style={{color: 'white'}}
-              name="call-end"
-              size={20}/>
-          </TouchableOpacity>
+        <View style={[VideoCallingStyle.buttonView]}>
+          <NeoButton
+            width={wp('17')}
+            height={hp('9')}
+            backgroundColor={AppColor.whiteShade}
+            borderRadius={wp('25')}>
+            <TouchableOpacity>
+              <MaterialIcon
+                style={{color: AppColor.red}}
+                name="call-end"
+                size={29}
+              />
+            </TouchableOpacity>
+          </NeoButton>
         </View>
 
         <View style={VideoCallingStyle.buttonView}>
-          <TouchableOpacity>
-            <Icon style={{color: 'white'}} name="video-camera" size={20}></Icon>
-          </TouchableOpacity>
+          <NeoButton
+            width={wp('17')}
+            height={hp('9')}
+            backgroundColor={AppColor.whiteShade}
+            borderRadius={wp('25')}>
+            <TouchableOpacity>
+              <Icon
+                style={{color: AppColor.black}}
+                name="video-camera"
+                size={22}></Icon>
+            </TouchableOpacity>
+          </NeoButton>
         </View>
       </View>
     </SafeAreaView>
