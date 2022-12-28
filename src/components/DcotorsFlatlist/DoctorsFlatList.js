1b1996246466f7179d8ccfdabfddee10fb9c2a83
hamad4596 <181370131@gift.edu.pk>
2022-12-28 12:12:06 +0500

    28-Dec-2022

--- src/components/DcotorsFlatlist/DoctorsFlatList.js
+++ src/components/DcotorsFlatlist/DoctorsFlatList.js
@@ -9,9 +9,12 @@ const DoctorsFlatList = ({horizontal, numColumns, marginRight, filterFuction}) =
   const [doctorInfo, setDoctorInfo]= useState([
     {name: 'Dr. Amanda', dept: 'Dentist', star: "4.5", experience: '5 years', color: 'rgba(195, 140, 222, 0.5)', source: require('../../assets/images/doctorsInfo.png')},
     {name: 'Dr. Omer', dept: 'Urology', star: "4.0", experience: '7 years', color: 'rgba(247, 106, 106, 0.4)', source: require('../../assets/images/drOmer.png')},
-    {name: 'Dr. Asnan', dept: 'Cardiology', star: "3.5", experience: '10 years', color: 'rgba(195, 140, 222, 1)', source: require('../../assets/images/doctorsInfo.png')},
+    {name: 'Dr. Asnan', dept: 'Radiology', star: "3.5", experience: '10 years', color: 'rgba(195, 140, 222, 1)', source: require('../../assets/images/doctorsInfo.png')},
     {name: 'Dr. Gough', dept: 'Cardiology', star: "2.5", experience: '15 years', color: 'rgba(195, 140, 222, 0.1)', source: require('../../assets/images/doctorsInfo.png')},
-    {name: 'Dr. Abdullah', dept: 'Gynacology', star: "4.5", experience: '2 years', color: 'rgba(247, 106, 106, 0.4)', source: require('../../assets/images/doctorsInfo.png')},
+    {name: 'Dr. Abdullah', dept: 'Gynacology', star: "4.5", experience: '2 years', color: 'rgba(195, 140, 222, 0.5)', source: require('../../assets/images/doctorsInfo.png')},
+    {name: 'Dr. Imran', dept: 'Gynacology', star: "4.5", experience: '2 years', color: 'rgba(247, 106, 106, 0.4)', source: require('../../assets/images/doctorsInfo.png')},
+    {name: 'Dr. Saqib', dept: 'Radiology', star: "4.5", experience: '2 years', color: 'rgba(195, 140, 222, 0.5)', source: require('../../assets/images/doctorsInfo.png')},
+    {name: 'Dr. Umair', dept: 'Gynacology', star: "4.5", experience: '2 years', color: 'rgba(195, 140, 222, 0.1)', source: require('../../assets/images/doctorsInfo.png')},
   ])
 
   const filteredArray = doctorInfo.filter(item => item.dept === selectedCategoryResult);
